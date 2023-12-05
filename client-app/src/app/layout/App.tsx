16d26524f55ba3552231b920af2cffc2e5d7e1d1
Alexander Matsinovich <sashamacinovich@gmail.com>
2023-12-05 16:20:32 +0300

    Fixed data response? added loading screen

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode  , setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     agent.Activities.list().then(response => {
@@ -18,8 +19,9 @@ function App() {
       response.forEach((activity: Activity) => {
         activity.date = activity.date.split('T')[0];
         activities.push(activity);
-      });
-      setActivities(response.data)
+      })
+      setActivities(response);
+      setLoading(false);
     })
   }, [])
 
@@ -49,6 +51,8 @@ function App() {
   function handleDeleteActivity(id: string) {
     setActivities([...activities.filter(x => x.id !== id)])
   }
+
+  if (loading) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
