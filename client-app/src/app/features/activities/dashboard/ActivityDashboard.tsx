efbb0641f77146692a656ba3e4f1e76619f52bf4
Alexander Matsinovich <sashamacinovich@gmail.com>
2023-12-12 17:28:12 +0300

    Added Routing? refactored some events to use links

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,11 +4,20 @@ import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../stores/store";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../../layout/LoadingComponent";
 
 
 export default observer (function ActivityDashboard() {
-    
+
     const {activityStore} = useStore();
+
+    useEffect(() => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+  
     const {selectedActivity, editMode} = activityStore;
     
     return (
