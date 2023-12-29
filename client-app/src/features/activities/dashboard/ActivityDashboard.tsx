aa19210d5a051d63d59ffa095cb497293f652786
Maramicodick <sashamacinovich@gmail.com>
2023-12-29 16:20:39 +0300

    implemented client side registration ang login

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -16,7 +16,7 @@ export default observer (function ActivityDashboard() {
       if (activityRegistry.size === 0) loadActivities();
     }, [loadActivities, activityRegistry.size])
   
-    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loadinging activities'/>
     
     return (
         <Grid>
